b502d98b3b775c78f41d8429cafbe2af7a0871ae
Gary Tokman <hello@garytokman.me>
2021-04-24 15:30:05 -0400

    feat: add new View extensions

---
+++ Sources/ExtensionKit/SwiftUI/RoundedCorner.swift
@@ -0,0 +1,15 @@
+#if canImport(SwiftUI)
+
+import SwiftUI
+
+#endif
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
--- Sources/ExtensionKit/SwiftUI/View.swift
+++ Sources/ExtensionKit/SwiftUI/View.swift
@@ -1,4 +1,8 @@
+#if canImport(SwiftUI)
+
 import SwiftUI
+
+#endif
 
 @available(iOS 13.0, *)
 public extension View {
@@ -17,4 +21,75 @@ public extension View {
         )
     }
 
+    /// If a condition is true, transform and return a new view
+    /// - Parameters:
+    ///   - condition: condition
+    ///   - transform: transformation if true
+    /// - Returns: new view
+    @ViewBuilder
+    func `if`<Transform: View>(_ condition: Bool, transform: (Self) -> Transform) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
+    
+    /// If a condition is true, transform and return a new view
+    /// - Parameters:
+    ///   - condition: condition
+    ///   - ifTransform: true case transform
+    ///   - elseTransform: else case transform
+    /// - Returns: new view
+    @ViewBuilder
+    func `if`<TrueContent: View, FalseContent: View>(
+      _ condition: Bool,
+      if ifTransform: (Self) -> TrueContent,
+      else elseTransform: (Self) -> FalseContent
+    ) -> some View {
+      if condition {
+        ifTransform(self)
+      } else {
+        elseTransform(self)
+      }
+    }
+    
+    /// Embed Self in `NavigationView`
+    /// - Returns: New view with `NavigationView` parent
+    func embedInNavigationView() -> some View {
+        NavigationView { self }
+    }
+    
+    /// Round view with specific corners
+    /// - Parameters:
+    ///   - radius: radius
+    ///   - corners: corners to round
+    /// - Returns: new rounded view
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+
+    /// Debug print view representation
+    /// - Returns: Self after printing
+    func debug() -> Self {
+        print(Mirror(reflecting: self).subjectType)
+        return self
+    }
+
+    /// Erase view to `AnyView`
+    /// - Returns: new view
+    func eraseToAnyView() -> AnyView{
+        AnyView(self)
+    }
+    
+    /// Hides the view conditionally
+    /// - Parameter hidden: is hidden
+    /// - Returns: hidden view but not drawn on screen
+    @ViewBuilder
+    func hidden(_ hidden: Bool) -> some View {
+        switch hidden {
+        case true: self.hidden()
+        case false: self
+        }
+    }
 }
