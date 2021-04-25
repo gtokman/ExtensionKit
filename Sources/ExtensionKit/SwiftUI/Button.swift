09a0bebf1a61a29dfed6779729e1b758732eef37
Gary Tokman <hello@garytokman.me>
2021-04-25 14:13:38 -0400

    feat: add Button helper initializers

---
+++ Sources/ExtensionKit/SwiftUI/Button.swift
@@ -0,0 +1,28 @@
+#if canImport(SwiftUI)
+
+import SwiftUI
+
+#endif
+
+public extension Button where Label == Image {
+    
+    /// Initialize button with system image and trailing action
+    /// - Parameters:
+    ///   - systemName: System image name
+    ///   - action: action
+    init(systemName: String, action: @escaping () -> Void) {
+        self.init(action: action) {
+            Image(systemName: systemName)
+        }
+    }
+    
+    /// Initialize button with local image and trailing action
+    /// - Parameters:
+    ///   - systemName: System image name
+    ///   - action: action
+    init(imageName: String, action: @escaping () -> Void) {
+        self.init(action: action) {
+            Image(imageName)
+        }
+    }
+}
