55e753fcb1376810616fd7ca18420fc9b92b93d9
Gary Tokman <hello@garytokman.me>
2021-04-25 14:12:43 -0400

    feat: add Image initializers

---
+++ Sources/ExtensionKit/SwiftUI/Image.swift
@@ -0,0 +1,32 @@
+#if canImport(SwiftUI)
+
+import SwiftUI
+
+#endif
+
+public extension Image {
+    
+    /// Initialize with a default image
+    /// - Parameters:
+    ///   - name: primary image name
+    ///   - defaultImage: default image name
+    init(_ name: String, defaultImage: String) {
+        if let img = UIImage(named: name) {
+            self.init(uiImage: img)
+        } else {
+            self.init(defaultImage)
+        }
+    }
+
+    /// Initialize with default system image
+    /// - Parameters:
+    ///   - name: primary image name
+    ///   - defaultSystemImage: default image name
+    init(_ name: String, defaultSystemImage: String) {
+        if let img = UIImage(named: name) {
+            self.init(uiImage: img)
+        } else {
+            self.init(systemName: defaultSystemImage)
+        }
+    }
+}
