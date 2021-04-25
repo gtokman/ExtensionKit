e4ae44005614cd936a926db4e901b0752c0062b6
Gary Tokman <hello@garytokman.me>
2021-04-25 01:31:43 -0400

    feat: add shimmer view and modifier

---
+++ Sources/ExtensionKit/SwiftUI/ShimmerView.swift
@@ -0,0 +1,91 @@
+#if canImport(SwiftUI)
+
+import SwiftUI
+
+#endif
+
+/// View with linear gradient shimmer animation
+struct ShimmerView: View {
+    @State var show: Bool = false
+    
+    let speed: Double
+    let angle: Angle
+    
+    var gradient: LinearGradient {
+        let leading = Gradient.Stop(color: .clear, location: 0.35)
+        let center = Gradient.Stop(color: Color.white.opacity(0.7), location: 0.45)
+        let trailing = Gradient.Stop(color: .clear, location: 0.55)
+        
+        return LinearGradient(
+            gradient: .init(stops: [leading, center, trailing]),
+            startPoint: .top,
+            endPoint: .bottom
+        )
+    }
+    
+    var animation: Animation {
+        Animation
+            .default
+            .speed(0.15)
+            .delay(0)
+            .repeatForever(autoreverses: false)
+    }
+    
+   var body: some View {
+        GeometryReader { geo in
+            Color.white
+                .mask(Rectangle().fill(self.gradient))
+                .padding(-self.calcSize(geo))
+                .rotationEffect(self.angle)
+                .offset(x: self.calcOffset(geo), y: 0)
+                .animation(self.animation)
+        }
+    }
+    
+    init(speed: Double, angle: Angle) {
+        self.speed = speed
+        self.angle = angle
+        self.show.toggle()
+    }
+    
+    func calcOffset(_ geo: GeometryProxy) -> CGFloat {
+        let size = calcSize(geo)
+        return (self.show ? size : -size) * 2
+    }
+    
+    func calcSize(_ geo: GeometryProxy) -> CGFloat {
+        return max(geo.size.width, geo.size.height)
+    }
+}
+
+/// Shimmer view modifier
+struct ShimmerModifier: ViewModifier {
+    let isActive: Bool
+    let speed: Double
+    let angle: Angle
+    
+    public func body(content: Content) -> some View {
+        if(!isActive) { return content.eraseToAnyView() }
+        
+        let view = content
+            .opacity(0.3)
+            .overlay(
+                ShimmerView(
+                    speed: speed,
+                    angle: angle
+                ).mask(content)
+        )
+        
+        return view.eraseToAnyView()
+    }
+    
+    public init(
+         isActive: Bool,
+         speed: Double,
+         angle: Angle
+    ) {
+         self.isActive = isActive
+         self.speed = speed
+         self.angle = angle
+     }
+}
