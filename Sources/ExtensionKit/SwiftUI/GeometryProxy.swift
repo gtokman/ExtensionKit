08e0edbc47c58432c200fddb40f2d65c92918cba
Gary Tokman <hello@garytokman.me>
2021-04-25 14:12:33 -0400

    feat: add geometry proxy extensions

---
+++ Sources/ExtensionKit/SwiftUI/GeometryProxy.swift
@@ -0,0 +1,44 @@
+#if canImport(SwiftUI)
+
+import SwiftUI
+
+#endif
+
+@available(iOS 13.0, OSX 10.15, *)
+public extension GeometryProxy {
+    var localFrame: CGRect { frame(in: .local) }
+    
+    var localWidth: CGFloat { localFrame.width }
+    
+    var localHeight: CGFloat { localFrame.height }
+    
+    var localCenter: CGPoint { .init(x: localFrame.midX, y: localFrame.midY) }
+    
+    var localTop: CGFloat { localFrame.minY }
+    
+    var localBottom: CGFloat { localFrame.maxY }
+    
+    var localLeft: CGFloat { localFrame.minX }
+    
+    var localRight: CGFloat { localFrame.maxX }
+    
+    var localDiameter: CGFloat { return min(localWidth, localHeight) }
+    
+    var globalFrame: CGRect { frame(in: .global) }
+    
+    var globalWidth: CGFloat { globalFrame.width }
+    
+    var globalHeight: CGFloat { globalFrame.height }
+    
+    var globalCenter: CGPoint { .init(x: globalFrame.midX, y: globalFrame.midY) }
+    
+    var globalTop: CGFloat { globalFrame.minY }
+    
+    var globalBottom: CGFloat { globalFrame.maxY }
+    
+    var globalLeft: CGFloat { globalFrame.minX }
+    
+    var globalRight: CGFloat { globalFrame.maxX }
+    
+    var globalDiameter: CGFloat { return min(globalWidth, globalHeight) }
+}
