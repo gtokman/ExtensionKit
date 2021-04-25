3482fbb994cfff12808b301183327d200708e243
Gary Tokman <hello@garytokman.me>
2021-04-25 14:12:00 -0400

    feat: add gradient extension to shape

---
+++ Sources/ExtensionKit/SwiftUI/Shape.swift
@@ -0,0 +1,26 @@
+#if canImport(SwiftUI)
+
+import SwiftUI
+
+#endif
+
+@available(iOS 13.0, OSX 10.15, *)
+public extension Shape {
+    
+    /// Addd a `LinearGradient` fill on Shape
+    /// - Parameters:
+    ///   - colors: Colors
+    ///   - start: Start, default top
+    ///   - end: End, default bottom
+    /// - Returns: View
+    func gradientFill(
+        colors: Color...,
+        start: UnitPoint = .top,
+        end: UnitPoint = .bottom) -> some View {
+            fill(
+                LinearGradient(
+                    gradient: Gradient(colors: colors),
+                    startPoint: start, endPoint: end)
+            )
+    }
+}
