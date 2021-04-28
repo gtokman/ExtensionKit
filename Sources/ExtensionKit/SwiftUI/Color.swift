451499b62c9b65360f49251bbf423f2d3b81dad5
Gary Tokman <hello@garytokman.me>
2021-04-28 00:22:19 -0400

    feat: add color inverted property and more color helpers

--- Sources/ExtensionKit/SwiftUI/Color.swift
+++ Sources/ExtensionKit/SwiftUI/Color.swift
@@ -1,17 +1,27 @@
-#if canImport(SwiftUI)
-
 import SwiftUI
-
-#endif
-
-#if canImport(UIKit)
-
 import UIKit
-
-#endif
 
 @available(iOS 13.0, macOS 10.15, *)
 public extension Color {
+    
+    /// Initialize with HEX
+    /// - Parameter hex: HEX UInt value
+    /// - Returns: Color
+    static func hex(_ hex: UInt, alpha: Double = 1) -> Self {
+      Self(
+        red: Double((hex & 0xff0000) >> 16) / 255,
+        green: Double((hex & 0x00ff00) >> 8) / 255,
+        blue: Double(hex & 0x0000ff) / 255,
+        opacity: alpha
+      )
+    }
+    
+    /// Initialize with HEX
+    /// - Parameter hex: HEX String value
+    /// - Returns: Color
+    static func hex(_ hex: String, alpha: CGFloat = 1) -> Self {
+        self.init(UIColor(hexCode: hex, alpha: alpha))
+    }
     
     /// Initialize with HEX
     /// - Parameter hex: HEX
@@ -44,5 +54,14 @@ public extension Color {
         let uiColor = UIColor(self)
         let adjustedColor = uiColor.darken(by: percentage)
         return Color(adjustedColor)
+    }
+    
+    @available(iOS 14.0, *)
+    /// Inverse color
+    var inverted: Color {
+        var a: CGFloat = 0.0, r: CGFloat = 0.0, g: CGFloat = 0.0, b: CGFloat = 0.0
+        return UIColor(self).getRed(&r, green: &g, blue: &b, alpha: &a)
+            ? Color(UIColor(red: 1.0-r, green: 1.0-g, blue: 1.0-b, alpha: a))
+            : .black
     }
 }
