038c09ae286d4f3722452ec9f7d52f1ff98b6f9d
Gary Tokman <hello@garytokman.me>
2021-05-01 11:46:34 -0400

    feat: new text initializers for Date

--- Sources/ExtensionKit/SwiftUI/Text.swift
+++ Sources/ExtensionKit/SwiftUI/Text.swift
@@ -2,6 +2,27 @@ import SwiftUI
 
 public extension Text {
     
+    /// Dateformatter
+    static let dateFormatter = DateFormatter()
+    
+    /// Create Text from date formate
+    /// - Parameters:
+    ///   - date: `Date`
+    ///   - dateStyle: `DateFormatter.Style`, default .full
+    init(_ date: Date, dateStyle: DateFormatter.Style = .full) {
+        let formatter = Text.dateFormatter
+        formatter.dateStyle = dateStyle
+        self.init(verbatim: formatter.string(from: date))
+    }
+
+    /// Create Text with date formatter string
+    /// - Parameters:
+    ///   - date: `Date`
+    ///   - formatter: `DateFormatter`
+    init(_ date: Date, formatter: DateFormatter = Text.dateFormatter) {
+      self.init(verbatim: formatter.string(from: date))
+    }
+    
     /// Text with system font
     /// - Parameters:
     ///   - size: size, default 18
@@ -21,7 +42,10 @@ public extension Text {
     func monospaced(_ size: CGFloat = 18, weight: SwiftUI.Font.Weight = .regular, design: SwiftUI.Font.Design = .monospaced) -> Text {
         self.font(.system(size: size, weight: weight, design: design))
     }
-    
+
+    /// Hidden `View` with frame of String vale
+    /// - Parameter value: String
+    /// - Returns: View
     func templateSize(for value: String) -> some View {
         return Text(value).hidden().overlay(self)
     }
