3075954066ef4cd93782e8b74822d071366d4eca
Gary Tokman <hello@garytokman.me>
2021-05-03 08:29:36 -0400

    feat: add sink Result variant to Combine extensions

--- Sources/ExtensionKit/Combine/Publisher.swift
+++ Sources/ExtensionKit/Combine/Publisher.swift
@@ -7,6 +7,19 @@ public extension Publisher where Failure == Never {
     func receiveOnMain() -> Publishers.ReceiveOn<Self, DispatchQueue> {
         self.receive(on: DispatchQueue.main)
     }
-    
-    
+
+    /// A single value sink function outputs a `Result`
+    /// - Parameter result: Result<Output, Failure>
+    /// - Returns: AnyCancellable
+    func sink(result: @escaping ((Result<Self.Output, Self.Failure>) -> Void)) -> AnyCancellable {
+        return sink(receiveCompletion: { completion in
+            switch completion {
+            case .failure(let error):
+                    result(.failure(error))
+            case .finished: break
+            }
+        }, receiveValue: { output in
+            result(.success(output))
+        })
+    }
 }
