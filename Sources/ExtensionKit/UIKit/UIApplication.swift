68e26e536a64fd547238c55763b351c69adab2bf
Gary Tokman <hello@garytokman.me>
2021-05-06 09:49:47 -0400

    feat: add network reachability check

--- Sources/ExtensionKit/UIKit/UIApplication.swift
+++ Sources/ExtensionKit/UIKit/UIApplication.swift
@@ -1,6 +1,7 @@
 #if canImport(UIKit)
 
     import UIKit
+    import SystemConfiguration
 
 #endif
 
@@ -113,5 +114,32 @@ public extension UIApplication {
             }
             return
         }
+    }
+    
+    /// Checks if the application is connected to the internet via `SystemConfiguration`
+    static var isConnectedToNetwork: Bool {
+        
+        var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) { zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        
+        var flags: SCNetworkReachabilityFlags = SCNetworkReachabilityFlags(rawValue: 0)
+        guard SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) else {
+            dprint("Network is not reachable.")
+            return false
+        }
+        
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        let ret = (isReachable && !needsConnection)
+        
+        return ret
+        
     }
 }
