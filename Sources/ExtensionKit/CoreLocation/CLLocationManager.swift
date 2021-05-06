6672ed8faa15e62f5a2b17cec4baee5a1ac54cd9
Gary Tokman <hello@garytokman.me>
2021-05-05 23:15:27 -0400

    feat: add docs for CLLocation authorization

--- Sources/ExtensionKit/CoreLocation/CLLocationManager.swift
+++ Sources/ExtensionKit/CoreLocation/CLLocationManager.swift
@@ -3,6 +3,41 @@ import CoreLocation
 
 public extension CLLocationManager {
     
+    /*
+     ```
+     class LocationStore: ObservableObject {
+         
+         @Published var coordinate: CLLocationCoordinate2D = .zero
+         @Published var status: CLAuthorizationStatus = .notDetermined
+         
+         let manager = CLLocationManager()
+         
+         var cancellables = Set<AnyCancellable>()
+         
+         func requestPermission() {
+             CLLocationManager
+                 .requestLocationAuthorization(with: manager, type: .whenInUse)
+                 .assign(to: \.status, on: self)
+                 .store(in: &cancellables)
+         }
+         
+         func getLocation() {
+             CLLocationManager
+                 .receiveLocationUpdates(from: manager)
+                 .compactMap(\.last)
+                 .map(\.coordinate)
+                 .sink { result in
+                     if case let .failure(error) = result {
+                         dprint("Error: \(error.localizedDescription)")
+                     }
+                 } receiveValue: { coordinate in
+                     self.coordinate = coordinate
+                 }
+                 .store(in: &cancellables)
+         }
+     }
+     ```
+     **/
     /// Request locaton authorization and subscribe to `CLAuthorizationStatus` updates
     /// - Parameters:
     ///   - manager: `CLLocationManager`
