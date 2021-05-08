79744902a1084e37f8235c7a47c82f017b393b95
Gary Tokman <hello@garytokman.me>
2021-05-08 12:02:23 -0400

    chore: update the build command

--- lgtm.sh
+++ lgtm.sh
@@ -1,4 +1,4 @@
-swift build
+swift build -Xswiftc "-sdk" -Xswiftc "`xcrun --sdk iphonesimulator --show-sdk-path`" -Xswiftc "-target" -Xswiftc "x86_64-apple-ios14.0-simulator"
 sourcedocs generate --all-modules
 git add . && git commit -m "chore: update docs"
 npm run release
